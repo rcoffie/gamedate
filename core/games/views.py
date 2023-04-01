ec94af46dd261f3ccfbd01f9f4cf18159d680999
Robert Coffie <rcoffie22@yahoo.com>
2023-04-01 17:36:29 +0000

    adding djangodecouple and project template

---
+++ core/games/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+import requests
+import json 
+# Create your views here.
+
+url = 'https://www.mmobomb.com/api1/games'
+r = requests.get(url)
+
+
+def home(request):
+
+    data = json.loads(r.text)
+    context = {'data':data}
+    return render(request, 'games/home.html',context)
